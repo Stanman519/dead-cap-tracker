6c19a4b497795d167606644f95701982a102eedb
RyanStanley <Ryan.Stanley@dcsg.com>
2021-02-10 15:43:33 -0500

    added ssl.mode require

--- DeadCapTracker/Startup.cs
+++ DeadCapTracker/Startup.cs
@@ -71,7 +71,9 @@ namespace DeadCapTracker
                 Port = databaseUri.Port,
                 Username = userInfo[0],
                 Password = userInfo[1],
-                Database = databaseUri.LocalPath.TrimStart('/')
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require, 
+                TrustServerCertificate = true
             };
 
 
