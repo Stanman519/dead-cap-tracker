ac3e7314724abce08a694db6c8b1be60cdd96915
RyanStanley <Ryan.Stanley@dcsg.com>
2021-04-05 16:40:46 -0400

    mapping

--- DeadCapTracker/Profiles/TransactionProfile.cs
+++ DeadCapTracker/Profiles/TransactionProfile.cs
@@ -7,7 +7,28 @@ namespace DeadCapTracker.Profiles
     {
         public TransactionProfile()
         {
-            CreateMap<TransactionDTO, Transaction>();
+            CreateMap<TransactionDTO, Transaction>()
+                .ForMember(dest => dest.Transactionid,
+                opts => opts.MapFrom(src => src.TransactionId))
+                .ForMember(dest => dest.Timestamp,
+                    opts => opts.MapFrom(src => src.Timestamp))
+                .ForMember(dest => dest.Franchiseid,
+                    opts => opts.MapFrom(src => src.FranchiseId))
+                .ForMember(dest => dest.Salary,
+                    opts => opts.MapFrom(src => src.Salary))
+                .ForMember(dest => dest.Amount,
+                    opts => opts.MapFrom(src => src.Amount))
+                .ForMember(dest => dest.Playername,
+                    opts => opts.MapFrom(src => src.PlayerName))
+                .ForMember(dest => dest.Position,
+                    opts => opts.MapFrom(src => src.Position))
+                .ForMember(dest => dest.Team,
+                    opts => opts.MapFrom(src => src.Team))
+                .ForMember(dest => dest.Years,
+                    opts => opts.MapFrom(src => src.Years))
+                .ForMember(dest => dest.Yearoftransaction,
+                    opts => opts.MapFrom(src => src.YearOfTransaction))
+                ;
         }
     }
 }
